3ec5053203f47f156e612a8dcf07bbff74f138c8
yinyue <yinyue1212@hotmail.com>
2018-02-07 16:35:36 +0800

    1

--- app/Components/ClientManager.php
+++ app/Components/ClientManager.php
@@ -1,72 +1,77 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: HappyQi
  * Date: 2017/9/28
  * Time: 10:30
  */
-
 namespace App\Components;
-
 use App\Models\AD;
 use App\Models\Client;
 use Qiniu\Auth;
-
 class ClientManager
 {
     /*
-     * 根据客户号码查询客户信息
+     * 根据id获取客户信息
      *
      * By TerryQi
      *
-     * 2017-11-27
-     *
-     */
-    public static function getByPhonenum($phonenum)
-    {
-        $client = Client::where('telephone', '=', $phonenum)->first();
-        return $client;
-    }
-
-
-    /*
-     * 根据id获取客户信息
-     *
-     *
-     *
+     * 2018-01-20
      */
     public static function getById($id)
     {
-        $client = Client::where('id', '=', $id)->first();
+        $client = Client::find($id);
         return $client;
     }
-
     /*
-     * 获取全部老客户信息，分页
+     * 根据phonenum获取客户信息
      *
+     * By TerryQi
+     *
+     * 2018-02-01
+     */
+    public static function getByPhonenum($phonenum)
+    {
+        $client = Client::where('phonenum', '=', $phonenum)->first();
+        return $client;
+    }
+    /*
+     * 获取列表
+     *
+     * By TerryQi
+     */
+    public static function getList()
+    {
+        $clients = Client::orderby('id', 'desc')->get();
+        return $clients;
+    }
+    /*
+     * 获取列表-分页
+     *
+     * By TerryQi
      *
      */
     public static function getListPaginate()
     {
         $clients = Client::orderby('id', 'desc')->paginate(Utils::PAGE_SIZE);
-        return;
+        return $clients;
     }
-
     /*
-     * 获取全部饿虎
+     * 增加客户报备次数
      *
+     * By TerryQi
      *
      */
-    public static function getList()
+    public static function addBaobeiTimes($client_id)
     {
-        $clients = Client::orderby('id', 'desc')->get();
-        return;
+        $client = self::getById($client_id);
+        if ($client) {
+            $client->baobei_times += 1;
+            $client->save();
+        }
     }
-
-
     /*
-     * 设置客户信息，用于编辑、
+     * 设置客户信息，用于编辑
      *
      * By TerryQi
      *
@@ -76,8 +81,20 @@ class ClientManager
         if (array_key_exists('name', $data)) {
             $client->name = array_get($data, 'name');
         }
-        if (array_key_exists('telephone', $data)) {
-            $client->telephone = array_get($data, 'telephone');
+        if (array_key_exists('phonenum', $data)) {
+            $client->phonenum = array_get($data, 'phonenum');
+        }
+        if (array_key_exists('status', $data)) {
+            $client->status = array_get($data, 'status');
+        }
+        if (array_key_exists('user_id', $data)) {
+            $client->user_id = array_get($data, 'user_id');
+        }
+        if (array_key_exists('baobei_times', $data)) {
+            $client->baobei_times = array_get($data, 'baobei_times');
+        }
+        if (array_key_exists('remark', $data)) {
+            $client->remark = array_get($data, 'remark');
         }
         return $client;
     }
